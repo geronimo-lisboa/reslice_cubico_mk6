f923f6e5a2b5d737cdffac56cb012eeb1ec3f5e0
luciano <luciano.geronimo.fnord@gmail.com>
2018-03-21 10:09:50 -0300

    Está fazendo o reslice inicial. Agora é por o cubo.

--- main.cpp
+++ main.cpp
@@ -34,13 +34,50 @@ public:
 int main(int argc, char** argv) {
 	///Carga da imagem
 	ObserveLoadProgressCommand::Pointer prog = ObserveLoadProgressCommand::New();
-	const std::string txtFile = "C:\\meus dicoms\\abdomem-feet-first";//"C:\\meus dicoms\\Visible Human Male CT DICOM"; //"C:\\meus dicoms\\abdomem-feet-first";// "C:\\meus dicoms\\Marching Man";//"C:\\meus dicoms\\abdomem-feet-first";//"C:\\meus dicoms\\Marching Man";//"C:\\meus dicoms\\abdomem-feet-first";//"C:\\meus dicoms\\Marching Man"; //"C:\\meus dicoms\\abdomem-feet-first";
+	const std::string txtFile = "C:\\meus dicoms\\Uro teste_corte lira";
 	const std::vector<std::string> lst = GetList(txtFile);
 	std::map<std::string, std::string> metadataDaImagem;
 	itk::Image<short, 3>::Pointer imagemOriginal = LoadVolume(metadataDaImagem, lst, prog);
 	vtkSmartPointer<vtkImageImport> imagemImportadaPraVTK = CreateVTKImage(imagemOriginal);//importa a imagem da itk pra vtk.
 	imagemImportadaPraVTK->Update();
-	//Cria a tela do cubo
+	//Cria a tela do reslice cubico
+	auto rendererCubo = vtkSmartPointer<vtkRenderer>::New();
+	auto renderWindowCubo = vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindowCubo->AddRenderer(rendererCubo);
+	auto interactorCubo = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	renderWindowCubo->SetInteractor(interactorCubo);
+	rendererCubo->SetBackground(0.3, 0, 0);
+	renderWindowCubo->Render();
+	//A saída do reslice
+	auto imageActor = vtkSmartPointer<vtkImageActor>::New();
+	imageActor->GetProperty()->SetColorLevel(50);
+	imageActor->GetProperty()->SetColorWindow(350);
+	rendererCubo->AddActor(imageActor);
+	//O reslicer
+	auto reslicer = vtkSmartPointer<vtkImageSlabReslice>::New();
+	imageActor->GetMapper()->SetInputConnection(reslicer->GetOutputPort());
+	reslicer->SetInputConnection(imagemImportadaPraVTK->GetOutputPort());;
+	reslicer->AutoCropOutputOn();//Não sei se é on ou off o certo, mas sei que isso aqui controla se vai cortar a imagem ou se vai pegar ela toda.
+	auto resliceTransform = vtkSmartPointer<vtkTransform>::New();
+	resliceTransform->Translate(imagemImportadaPraVTK->GetOutput()->GetCenter());
+	reslicer->SetResliceTransform(resliceTransform);
+	reslicer->Update();
+	vtkImageData *resultado = reslicer->GetOutput();
+	if (resultado->GetExtent()[1] == -1)
+		throw "ta errado";
+	//pega o output e grava
+	boost::posix_time::ptime current_date_microseconds = boost::posix_time::microsec_clock::local_time();
+	long milliseconds = current_date_microseconds.time_of_day().total_milliseconds();
+	std::string filename = "C:\\reslice_cubico\\mk6\\dump\\" + boost::lexical_cast<std::string>(milliseconds)+".vti";
+	vtkSmartPointer<vtkXMLImageDataWriter> debugsave = vtkSmartPointer<vtkXMLImageDataWriter>::New();
+	debugsave->SetFileName(filename.c_str());
+	debugsave->SetInputConnection(reslicer->GetOutputPort());
+	debugsave->BreakOnError();
+	debugsave->Write();
+	//pega o output e exibe
+	imageActor->Update();
+	rendererCubo->ResetCamera();
+	renderWindowCubo->Render();
 	///////////////////////////////////////////////////
 	//A tela dummy PROS PROBLEMAS DO OPENGL
 	vtkSmartPointer<vtkRenderer> rendererDummy = vtkSmartPointer<vtkRenderer>::New();
